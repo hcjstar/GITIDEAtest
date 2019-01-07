f4d5d82af8c03cb2e66f5828cb349b01f08be602
hcjstar <he>
2019-01-07 10:00:19 +0800

    新增测试代码

---
+++ helloworld/src/com/troy/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.troy;
+
+/**
+ * @author hechaojun
+ * @Description: 测试hello
+ * @ClassName: HelloWorld
+ * @date 2019/1/7 9:58
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
